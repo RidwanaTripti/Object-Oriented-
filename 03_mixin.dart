96c8b7523df5994b6f68a5b1b32c2a773b2bc20a
tripti <ridwanaakter17@gmail.com>
2022-03-14 21:51:00 +0600

    mixin

--- 03_mixin.dart
+++ 03_mixin.dart
@@ -2,20 +2,20 @@ class Performer {
   void perform() => print("perform...");
 }
 
-mixin Dancer {
+mixin Dancer on Performer {
   void workasdancer() => print("dancer");
-  void perform() => workasdancer();
-  // void perform() => super.perform();
+  // void perform() => workasdancer();
+  void perform() => super.perform();
 }
 mixin Actor {
   void workasactor() => print("actor");
   void perform() => workasactor();
   // void perform() => super.perform();
 }
-mixin Singer on Performer {
+mixin Singer {
   void workassinger() => print("singer");
-  // void perform() => workassinger();
-  void perform() => super.perform();
+  void perform() => workassinger();
+  // void perform() => super.perform();
 }
 
 class Celebrety extends Performer with Dancer, Actor, Singer {}
